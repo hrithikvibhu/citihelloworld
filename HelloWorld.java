7a5ae4c652b73304618c10bdfaa7533e154654cc
hrithik singh <gmail@gmail.com>
2020-09-08 11:16:11 +0000

    new change

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,8 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World! from feature updated"); 
-    }
+	int i=0;
+	for(i = 0 ;i<5;i++){
+        	System.out.println("Hello, World! from feature updated"); 
+    	}
+}
 }
